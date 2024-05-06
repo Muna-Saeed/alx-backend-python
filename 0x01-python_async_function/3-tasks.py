02b5ae609d5c43c4424ccef05a4c2f3b7bf36590
Muna Saeed <munasaeed6367@gmail.com>
2024-05-06 21:31:32 +0300

    3. Tasks

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -13,9 +13,11 @@ def task_wait_random(max_delay: int) -> asyncio.Task:
     Regular function that returns an asyncio.Task.
 
     Args:
-        max_delay (int): Maximum delay in seconds for the wait_random coroutine.
+        max_delay (int): Maximum delay in seconds
+        for the wait_random coroutine.
 
     Returns:
-        asyncio.Task: Task representing the execution of the wait_random coroutine.
+        asyncio.Task: Task representing the execution
+        of the wait_random coroutine.
     """
     return asyncio.create_task(wait_random(max_delay))
