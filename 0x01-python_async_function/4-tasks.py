a7273d82b677de99309cdf802534940fb413e853
Muna Saeed <munasaeed6367@gmail.com>
2024-05-06 22:04:26 +0300

    4. Tasks

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+Tasks module
+"""
+
+
+import asyncio
+import random
+from typing import List
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    Asynchronous coroutine that spawns task_wait_random n times.
+
+    Args:
+        n (int): Number of times to spawn task_wait_random coroutine.
+        max_delay (int): Maximum delay in seconds
+        for each task_wait_random call.
+
+    Returns:
+        List[float]: List of delays in ascending order.
+    """
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
+    delays = await asyncio.gather(*tasks)
+    return delays
