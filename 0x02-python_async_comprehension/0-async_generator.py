cf1a2b0a4abbc5ebae9a9b25b7556c74f53823a7
Muna Saeed <munasaeed6367@gmail.com>
2024-05-07 16:56:40 +0300

    0. Async Generator

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -6,9 +6,10 @@ that yields random numbers between 0 and 10 with a one-second delay.
 
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Genarator:
     """
     An asynchronous generator that yields random numbers between 0 and 10
     with a one-second delay between each yield.
